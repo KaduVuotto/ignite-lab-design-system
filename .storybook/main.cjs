d89a3aae98822958ce46d3a89b18404a3f6c61c4
Kadu Vuotto <cev@lionx.com.br>
2022-10-22 14:35:55 -0300

    fix: Adjust in @storybook/addon-a11y

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -2,9 +2,9 @@ module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-a11y",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
   ],
   framework: "@storybook/react",
   core: {
